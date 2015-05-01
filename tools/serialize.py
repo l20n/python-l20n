fbf566e70ecb7381711c60219040d6a4ff999124
Zibi Braniecki <gandalf@mozilla.com>
2015-04-30 20:16:33 -0400

    update serializer to handle jsons

--- tools/serialize.py
+++ tools/serialize.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python 
 
-import sys
+import sys, os
 
 sys.path.append('./lib')
 import codecs
@@ -14,18 +14,23 @@ def read_file(path):
     return text
 
 def print_l20n(fileType, data):
-    print('----- ORIGINAL -----')
-    print(data)
-    l20nParser = l20n.format.parser.Parser()
-    print('----- AST -----')
-    ast = l20nParser.parse(data)
-    print(json.dumps(ast, indent=2))
-    print('--------------------')
     l20nSerializer = l20n.format.serializer.Serializer()
-    result = l20nSerializer.serialize(ast)
+    if fileType == 'json':
+        result = l20nSerializer.serialize(json.loads(data))
+    elif fileType == 'l20n':
+        print('----- ORIGINAL -----')
+        print(data)
+        l20nParser = l20n.format.parser.Parser()
+        print('----- AST -----')
+        ast = l20nParser.parse(data)
+        print(json.dumps(ast, indent=2))
+        print('--------------------')
+        result = l20nSerializer.serialize(ast)
+    
     print(result)
+
 if __name__ == "__main__":
-      file_type = 'l20n'
-      f = read_file(sys.argv[1])
-      print_l20n(file_type, f)
+    fileName, fileExtension = os.path.splitext(sys.argv[1])
+    f = read_file(sys.argv[1])
+    print_l20n(fileExtension[1:], f)
 
