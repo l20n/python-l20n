f2f2d9da971bf10575be176f7a69a7c469e058ad
Zibi Braniecki <gandalf@mozilla.com>
2015-06-25 10:37:27 -0700

    Make AST work with python 2

--- lib/l20n/format/ast.py
+++ lib/l20n/format/ast.py
@@ -8,7 +8,7 @@ def attr2json(attr):
     else:
         return attr
 
-class Node:
+class Node(object):
     def __init__(self):
         self.type = self.__class__.__name__
 
@@ -30,41 +30,41 @@ class Node:
 
 class Entry(Node):
     def __init__(self):
-        super().__init__()
+        super(Entry, self).__init__()
 
 class Identifier(Node):
     def __init__(self, name):
-        super().__init__()
+        super(Identifier, self).__init__()
         self.name = name
 
 class Variable(Node):
     def __init__(self, name):
-        super().__init__()
+        super(Variable, self).__init__()
         self.name = name
 
 class Global(Node):
     def __init__(self, name):
-        super().__init__()
+        super(Global, self).__init__()
         self.name = name
 
 class Value(Node):
     def __init__(self):
-        super().__init__()
+        super(Value, self).__init__()
 
 class String(Value):
     def __init__(self, source, content):
-        super().__init__()
+        super(String, self).__init__()
         self.source = source
         self.content = content
 
 class Hash(Value):
     def __init__(self, items):
-        super().__init__()
+        super(Hash, self).__init__()
         self.items = items
 
 class Entity(Entry):
     def __init__(self, id, value = None, index = None, attrs = None):
-        super().__init__()
+        super(Entity, self).__init__()
         self.id = id
         self.value = value
         self.index = index
@@ -72,46 +72,46 @@ class Entity(Entry):
 
 class Resource(Node):
     def __init__(self, body = None):
-        super().__init__()
+        super(Resource, self).__init__()
         self.body = body or []
 
 class Attribute(Node):
     def __init__(self, id, value, index = None):
-        super().__init__()
+        super(Attribute, self).__init__()
         self.id = id
         self.value = value
         self.index = index
 
 class HashItem(Node):
     def __init__(self, id, value, defItem):
-        super().__init__()
+        super(HashItem, self).__init__()
         self.id = id
         self.value = value
         self.default = defItem
 
 class Comment(Node):
     def __init__(self, body):
-        super().__init__()
+        super(Comment, self).__init__()
         self.body = body
 
 class Expression(Node):
     def __init__(self):
-        super().__init__()
+        super(Expression, self).__init__()
 
 class PropertyExpression(Expression):
     def __init__(self, idref, exp, computed = False):
-        super().__init__()
+        super(PropertyExpression, self).__init__()
         self.idref = idref
         self.exp = exp
         self.computed = computed
 
 class CallExpression(Expression):
     def __init__(self, callee, args):
-        super().__init__()
+        super(CallExpression, self).__init__()
         self.callee = callee
         self.args = args
 
 class JunkEntry(Node):
     def __init__(self, content):
-        super().__init__()
+        super(Node, self).__init__()
         self.content = content
