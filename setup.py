92e7cd0662a6198f15487f82a257dc06092bb30d
Matjaž Horvat <matjaz.horvat@gmail.com>
2016-05-11 21:53:01 +0200

    Add ftl support to setup file

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from distutils.core import setup
 import os
 
 setup(name='l20n',
-      version='0.1',
+      version='0.2',
       description='Python L20n library',
       author='Mozilla',
       author_email='team@mozilla.org',
       url='https://github.com/l20n/python-l20n',
-      packages=['l20n', 'l20n.format'],
+      packages=['l20n', 'l20n.format', 'ftl', 'ftl.format'],
      )
 
